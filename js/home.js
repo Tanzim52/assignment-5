3e7e4191ba7eb78c3154626c5abecacc2ab8b1f3
Tanzim52 <mahin1575@gmail.com>
2024-09-25 10:54:59 +0600

    extras

--- js/home.js
+++ js/home.js
@@ -6,7 +6,6 @@
 function getInputFieldValueById(id){
     const inputValue=document.getElementById(id).value
     const inputNumber=parseFloat(inputValue)
-    // console.log(inputNumber)
     return inputNumber;
     
     }
@@ -28,9 +27,11 @@ function getInputFieldValueById(id){
     
     // hidden
     function showSectionById(id){
+        
         // hide all the section
         document.getElementById('donate-section').classList.add('hidden')
         document.getElementById('history-section').classList.add('hidden')
+        
         // remove hidden
         document.getElementById(id).classList.remove('hidden')
     }
